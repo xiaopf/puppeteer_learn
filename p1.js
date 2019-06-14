103728fcda9c2ff1cbc23253c95782d7b824e2fb
xiaopf <326258842@qq.com>
2019-06-14 20:59:06 +0800

    update

--- p1.js
+++ p1.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
 const selector = '.WB_detail';
 (async () => {
-    const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1920, height: 1080 }});
+    const browser = await puppeteer.launch({ 
+        executablePath: './chromium/chrome.exe',
+        headless: false, 
+        defaultViewport: { width: 1920, height: 1080 }
+    });
     const page = await browser.newPage();
     await page.goto('https://weibo.com/p/1002061850988623/home?is_hot=1');
     // await page.waitForSelector('.WB_detail');
--- p3.js
+++ p3.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 var list = [];
 
 async function getDirectory(){
-    const browser = await (puppeteer.launch({ headless: true, devtools: true, defaultViewport: { width: 1000, height: 1080 } }));
+    const browser = await (puppeteer.launch({
+        executablePath: './chromium/chrome.exe',
+        headless: true, 
+        devtools: false, 
+        defaultViewport: { width: 1000, height: 1080 } 
+    }));
     const page = await browser.newPage();
     await page.goto('https://music.163.com/#/discover/playlist/?order=hot&cat=%E6%91%87%E6%BB%9A&limit=35&offset=0');
     const iframe = page.frames()[1];
@@ -23,11 +28,15 @@ async function getDirectory(){
     await browser.close();
 }
 
-async function getPlayList(item, i, len){
+async function getPlayList(item, index, len, listPart){
     try {
-        const browser = await (puppeteer.launch({ headless: true, devtools: true, defaultViewport: { width: 1000, height: 1080 } }));
+        const browser = await (puppeteer.launch({ 
+            executablePath: './chromium/chrome.exe', 
+            headless: true,  
+            defaultViewport: { width: 1000, height: 1080 } 
+        }));
         const page = await browser.newPage();
-        await page.goto(item.playListUrl);
+        await page.goto(item.playListUrl, { waitUntil: "networkidle2" });
         const iframe = page.frames()[1];
         let playList = await iframe.$$eval('tbody tr', els => {
             return new Promise(reslove => {
@@ -46,30 +55,61 @@ async function getPlayList(item, i, len){
                 reslove(singlePlayList)
             })
         });
-        list[i]['playList'] = playList;
+        
+        console.log(index+1)
+        listPart[index]['playList'] = playList;
         await browser.close();
     } catch (err) {
         console.log(err)
     }
-    console.clear();
-    console.log(Math.floor(i * 100/len) + '%');
+    // console.clear();
+    console.log(Math.floor((index+1) * 100/len) + '%');
 }
+
+
+function timeout(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+
+
+
+
 
 (async () => {
     await getDirectory();
+    
+    // let i = 0;
+
+    // let listPart = list.slice(0,3)
+
+
     let len = list.length;
-    let i = 0;
-    let timer = setInterval(async () => {
-        await getPlayList(list[i], i + 1, 35);
-        i ++;
-        if(i === 35){
-            clearInterval(timer);
-            console.log('完成！');
-        }
-    }, 5000);
+
+
+    // let timer = setInterval(async () => {
+    //     await getPlayList(listPart[i], i, len, listPart);
+    //     i ++;
+    //     if (i >= len){
+    //         clearInterval(timer);
+    //         let writerStream = fs.createWriteStream('./static/play_list.json');
+    //         writerStream.write(JSON.stringify(listPart), 'UTF8');
+    //         writerStream.end();
+    //         console.log('完成！');
+    //     }   
+    // }, 2000);
+
+    for (let i = 0 ; i < len; i ++) {
+        await getPlayList(list[i], i, len, list);
+        await timeout(2000);
+    }
+
     let writerStream = fs.createWriteStream('./static/play_list.json');
     writerStream.write(JSON.stringify(list), 'UTF8');
     writerStream.end();
+
 })();
 
 
